596ec08a6d280ae9f337f1485bdea7bd20f24ace
epochvisionng <epochvisionng@gmail.com>
2025-06-12 14:57:37 +0100

    alpha project

---
+++ assets/src/main.js
@@ -0,0 +1,4 @@
+document.addEventListener('scroll', (e)=>{
+    // console.log(e.target);
+     document.querySelector("#nav").style.cssText = "background-color: #fff; box-shadow: inset 0px 0px 5px gainsboro, 3px 3px 4px grey; border-radius:3rem; width:93%; margin:auto;top:7px;";
+})
